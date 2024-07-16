722900cf264e713c231795c511f07c377cf58ffd
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-07-16 16:20:57 +0200

    add dependance and streamlit

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import pandas as pd
+import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 from process_data import data_process
 
+
+st.title("Similarity Cosnus")
 data = pd.read_csv("data/data.csv")
 
-data["title"] = data["title"].apply(data_process)
 
-vectorizer = TfidfVectorizer()
 
-vectorizer_data = vectorizer.fit_transform(data["title"])
+# new_paper ="Efficient algorithms for large-scale machine learning."
+new_paper =  st.text_input("Give the title of your documents")
+new_paper_process = data_process(new_paper)
 
-new_paper = "Efficient algorithms for large-scale machine learning."
-vectorizer_new_paper = vectorizer.transform({new_paper})
-
-similarity_papers = cosine_similarity(vectorizer_data,vectorizer_new_paper).flatten()
-
-indice_papers = similarity_papers.argsort()[:-5][::-1]
-
-similarity_papers = data.iloc[indice_papers]
-
-print("Les papiers les plus similaires sont :")
-print(similarity_papers['title'])
+if st.button("new_paper"):
+    data["title"] = data["title"].apply(data_process)
+    vectorizer = TfidfVectorizer()
+    vectorizer_data = vectorizer.fit_transform(data["title"])
+    vectorizer_new_paper = vectorizer.transform([new_paper_process])
+    similarity_papers = cosine_similarity(vectorizer_new_paper,vectorizer_data).flatten()
+    indice_papers = similarity_papers.argsort()[:-6:-1]
+    papers = data.iloc[indice_papers]
+    st.write("Les papiers les plus similaires sont :")
+    st.write(papers)
 
